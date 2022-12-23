807cacf68e130250614bab314bba5625ffdf8a05
Masoud <mas.sam1368@gmail.com>
2022-12-23 22:09:51 +0330

    finish header and survay

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,16 +6,16 @@ const Header = () => {
         <h2 className="font-bold text-2xl pl-2 tracking-wide text-gray-800">Dashboard</h2>
       </div>
    
-        <form className="w-2/5 relative">
-          <input className="outline-1 outline-gray-400 w-full mt-1 py-1 bg-gray-200 border-solid border-gray-300 border-2 pl-4 rounded-md" type="text" placeholder="Search" />
-          <button className="rounded-md p-1 inline-block absolute right-1 top-2 bg-blue-500 text-white" type="submit">
-            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-          </button>
-        </form>
+      <form className="w-2/5 relative">
+        <input className="outline-1 outline-gray-400 w-full mt-1 py-1 bg-gray-200 border-solid border-gray-300 border-2 pl-4 rounded-md" type="text" placeholder="Search" />
+        <button className="rounded-md p-1 inline-block absolute right-1 top-2 bg-blue-500 text-white" type="submit">
+          <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        </button>
+      </form>
 
       <div className=" cursor-pointer pr-4 flex items-center border-gray-300 border-2 bg-gray-200 rounded-md">
-        <img className="rounded w-8 m-1" src="/images/profile-pic.jpg" alt="profile-pic" />
-        <span className="font-semibold mx-2">Prem Shahi</span>
+        <img className="rounded w-6 m-1" src="/images/profile-pic.jpg" alt="profile-pic" />
+        <span className="font-semibold mx-2 text-sm">Prem Shahi</span>
         <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" /></svg>
       </div>
     </header>
--- src/components/main/Survay.js
+++ src/components/main/Survay.js
@@ -1,59 +1,59 @@
 
 const Survay = () => {
   return (
-    <div className="flex space-x-14">
-        <div className="flex items-center">
+    <div className="flex justify-between  py-4 px-6">
+        <div className="flex items-center bg-white px-6 py-4 rounded-lg shadow-xl">
             <div className="flex flex-col">
-            <span>Total Order</span>
-            <span>40,876</span>
-            <span className="flex">
-                <img src="/icons/arrow-up.svg" />
-                <span>Up from yesterday</span>
-            </span>
+                <span className="font-bold text-lg">Total Order</span>
+                <span className="font-bold text-3xl">40,876</span>
+                <div className="flex mt-2">
+                    <svg className="w-4 h-4 bg-green-300 text-white rounded-full" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" /></svg>
+                    <span className="text-xs pl-1">Up from yesterday</span>
+                </div>
             </div>
-            <div>
-            <img className="w-6" src="/icons/trolley.svg" />   
+            <div className="bg-blue-100 text-blue-400 p-2 rounded-md ml-3">
+                <svg className="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>            
             </div>
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center bg-white px-6 py-4 rounded-lg shadow-xl">
             <div className="flex flex-col">
-            <span>Total Sales</span>
-            <span>40,876</span>
-            <span className="flex">
-                <img src="/icons/arrow-up.svg" />
-                <span>Up from yesterday</span>
-            </span>
+                <span className="font-bold text-lg">Total Sales</span>
+                <span className="font-bold text-3xl">21,876</span>
+                <div className="flex mt-2">
+                    <svg className="w-4 h-4 bg-green-300 text-white rounded-full" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" /></svg>
+                    <span className="text-xs pl-1">Up from yesterday</span>
+                </div>
             </div>
-            <div>
-            <img className="w-6" src="/icons/trolley.svg" />   
+            <div className="bg-green-100 text-green-400 p-2 rounded-md ml-3">
+                <svg className="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>            
             </div>
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center bg-white px-6 py-4 rounded-lg shadow-xl">
             <div className="flex flex-col">
-            <span>Total profit</span>
-            <span>40,876</span>
-            <span className="flex">
-                <img src="/icons/arrow-down.svg" />
-                <span>Up from yesterday</span>
-            </span>
+                <span className="font-bold text-lg">Total Profit</span>
+                <span className="font-bold text-3xl">17,886</span>
+                <div className="flex mt-2">
+                    <svg className="w-4 h-4 bg-red-300 text-white rounded-full" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" /></svg>
+                    <span className="text-xs pl-1">Up from yesterday</span>
+                </div>
             </div>
-            <div>
-            <img className="w-6" src="/icons/trolley.svg" />   
+            <div className="bg-yellow-100 text-yellow-400 p-2 rounded-md ml-3">
+                <svg className="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>            
             </div>
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center bg-white px-6 py-4 rounded-lg shadow-xl">
             <div className="flex flex-col">
-            <span>Total Return</span>
-            <span>40,876</span>
-            <span className="flex">
-                <img src="/icons/arrow-up.svg" />
-                <span>Up from yesterday</span>
-            </span>
+                <span className="font-bold text-lg">Total Return</span>
+                <span className="font-bold text-3xl">25,876</span>
+                <div className="flex mt-2">
+                    <svg className="w-4 h-4 bg-green-300 text-white rounded-full" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" /></svg>
+                    <span className="text-xs pl-1">Up from yesterday</span>
+                </div>
             </div>
-            <div>
-            <img className="w-6" src="/icons/trolley.svg" />   
+            <div className="bg-red-100 text-red-400 p-2 rounded-md ml-3">
+                <svg className="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>            
             </div>
-        </div>
+        </div> 
     </div>
     
   )
