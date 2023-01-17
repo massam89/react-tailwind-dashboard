dbe739695be027a7221eda06459e42766227b174
Masoud Sam <mas.sam1368@gmail.com>
2023-01-17 12:28:04 +0330

    work on cmd-env package and separate production and develpmnent

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/react-tailwind-dashboard">
+  <BrowserRouter basename={process.env.REACT_APP_BASE_URL}>
     <Provider store={store}>
       <React.StrictMode>
         <App />
