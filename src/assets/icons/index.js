66be69f67001eb0897ea0abf48a63d8612c3d30d
Masoud Sam <mas.sam1368@gmail.com>
2023-01-24 17:02:01 +0330

    change something

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -355,6 +355,24 @@ export const loaderIcon = (customClass) => {
     </svg>
   );
 };
+export const tickIcon = (customClass) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={`${customClass}`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+};
 export const eyeIcon = (customClass) => {
   return (
     <svg
@@ -369,6 +387,24 @@ export const eyeIcon = (customClass) => {
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+      />
+    </svg>
+  );
+};
+export const stopIcon = (customClass) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={`${customClass}`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
       />
     </svg>
   );
--- src/pages/login-register/Register.js
+++ src/pages/login-register/Register.js
@@ -1,13 +1,16 @@
 import FormInput from "../../components/formInput"
-import { arrowLeftIcon} from "../../assets/icons"
+import { arrowLeftIcon, stopIcon, tickIcon} from "../../assets/icons"
 import { useDispatch, useSelector } from "react-redux"
 import { uiActions } from "../../store/ui/uiSlice"
 import { checkUniqueUser } from "./_srv"
 import { useEffect, useState } from "react"
 import { toast } from "react-toastify"
 
+let initialLoad = true
+
 const Register = () => {
   const [username, setUsername] = useState('')
+  const [isUsernameValid, setIsUsernameValid] = useState(false)
   const disptach = useDispatch()
 
   const linkToLoginPageHandler = () => disptach(uiActions.changeLoginMode())
@@ -15,16 +18,23 @@ const Register = () => {
   const isLoginMode = useSelector(state => state.ui.loginMode)
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      checkUniqueUser({userName : username})
-      .then(res => {
-        if(res.data === true){
-          toast.error('username is unique!')
-        }
-      })
-      .catch(err => console.log(err))
-    }, 500);
-    return () => clearTimeout(timer)
+    if(!initialLoad){
+      
+      const timer = setTimeout(() => {
+        checkUniqueUser({userName : username})
+        .then(res => {
+          if(res.data === true){
+            setIsUsernameValid(true)
+          } else {
+            setIsUsernameValid(false)
+          }
+        })
+        .catch(err => toast.error('something wrong'))
+      }, 1000);
+      return () => clearTimeout(timer)
+    }
+    
+    initialLoad = false
   }, [username])
 
   const registerHandler = (e) => {
@@ -37,7 +47,8 @@ const Register = () => {
       <div className="w-3/4">
         <h2 className="text-gray-200 xs:text-gray-500 font-bold text-3xl mb-12">Get Started</h2>
         <form onSubmit={registerHandler}>
-          <div>
+          <div className="relative">
+            {!isUsernameValid ? tickIcon('w-7 h-7 absolute right-0 z-20 text-green-500') : stopIcon('w-7 h-7 absolute right-0 z-20 text-red-500')}
             <FormInput type='text' placeholder='Username' name='username' onChange={(e) => setUsername(e.target.value)} />
             <FormInput type='password' placeholder='Password' name='password' />          
           </div>
