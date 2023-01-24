b99af62c7b1d6b1636daa56dffb8e5857c4f963e
Masoud Sam <mas.sam1368@gmail.com>
2023-01-24 08:57:00 +0330

    responsive mobile part3

--- src/pages/login-register/index.js
+++ src/pages/login-register/index.js
@@ -7,7 +7,7 @@ const LoginRegister = () => {
     useEffect(() => {document.title = 'Dashboard App | Login-register'}, [])
 
     return (
-        <main className='h-screen relative z-50'>
+        <main className='h-screen relative z-50 overflow-hidden'>
             <Login />
             <Register />
             <Hero />
