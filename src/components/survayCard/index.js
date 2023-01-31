dedfa89d04ad479f552992ffc6305cdcaf771a55
Masoud <mas.sam1368@gmail.com>
2023-01-31 14:39:26 +0330

    fix bug in literal string in tailwind: really important tip

--- src/components/survayCard/index.js
+++ src/components/survayCard/index.js
@@ -1,6 +1,14 @@
 import { arrowDownIconWithTail, arrowUpIcon, trolleyIcon } from "../../assets/icons"
 
 const SurvayCard = ({card}) => {
+
+    const colorVariants = {
+        blue: 'bg-blue-100 text-blue-400',
+        red: 'bg-red-100 text-red-400',
+        yellow: 'bg-yellow-100 text-yellow-400',
+        green: 'bg-green-100 text-green-400'
+      }
+
     return (
         <div className="flex items-center justify-around sm:justify-around py-5 mb-3 md:justify-between bg-white hover:shadow-lg hover:shadow-blue-300 border-2 border-transparent hover:border-2 hover:border-blue-300 box-content transition-color duration-200 md:p-3 w-full sm:w-[calc(50%-15px)] lg:py-4 rounded-lg shadow-xl">
             <div className="flex flex-col">
@@ -11,7 +19,7 @@ const SurvayCard = ({card}) => {
                     <span className="text-xs pl-1">{card.isUp ? 'Up' : 'Down'} from yesterday</span>
                 </div>
             </div>
-            <div className={`bg-${card.trolleyColor}-100 text-${card.trolleyColor}-400 p-2 rounded-md ml-3`}>
+            <div className={`${colorVariants[card.trolleyColor]} p-2 rounded-md ml-3`}>
                 {trolleyIcon('w-5 lg:w-7')}
             </div>
         </div>
