a33b583e83359d7af5e33f13fada460070d9d31c
Masoud Sam <mas.sam1368@gmail.com>
2023-03-27 14:52:56 +0430

    disable reportVital

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./store";
 import 'react-toastify/dist/ReactToastify.css';
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename={process.env.REACT_APP_BASE_URL}>
